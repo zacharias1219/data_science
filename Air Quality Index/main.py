1f1cbb9b023b8c118177da373205f7c5ec7ca5b4
Richard Abishai <rickoshade1891@gmail.com>
2024-01-13 23:31:41 +0530

    first commit

--- Air Quality Index/main.py
+++ Air Quality Index/main.py
@@ -6,3 +6,13 @@ pio.templates.default = "plotly_white"
 
 data = pd.read_csv("delhiaqi.csv")
 print(data.head())
+
+data['date'] = pd.to_datetime(data['date'])
+
+fig = go.Figure()
+for pollutant in ['co', 'no', 'no2', 'o3', 'so2', 'pm2_5', 'pm10', 'nh3']:
+    fig.add_trace(go.Scatter(x=data['date'], y=data[pollutant], mode='lines', name=pollutant))
+
+fig.update_layout(title='Time Series Analysis of Air Pollutants in Delhi',xaxis_title='Date',yaxis_title='Concentration (µg/m³)')
+fig.show()
+
