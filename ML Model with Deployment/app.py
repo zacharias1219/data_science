73c556ad5705cf3c076f9b688bd6371134e23e4d
Richard Abishai <rickoshade1891@gmail.com>
2024-01-16 13:43:57 +0530

    last

--- ML Model with Deployment/app.py
+++ ML Model with Deployment/app.py
@@ -1,26 +1,25 @@
-from flask import Flask,renderer_template,url_for,request
+from flask import Flask,render_template,url_for,request
 import pandas as pd 
 import pickle
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.metrics import classification_report
-from sklearn.externals import joblib
 
 app=Flask(__name__)
 
 @app.route('/')
 def home():
-	return renderer_template('home.html')
+	return render_template('home.html')
 
 
 @app.route('/predict',methods=['POST'])
 def predict():
-    df = pd.read_csv('spam.csv', encoding="latin-1")
+    df = pd.read_csv('datasets_483_982_spam.csv', encoding="latin-1")
     df.drop(['Unnamed: 2', 'Unnamed: 3', 'Unnamed: 4'], axis=1, inplace=True)
-    df['label'] = df['class'].map({'ham': 0, 'spam': 1})
+    df['label'] = df['v1'].map({'ham': 0, 'spam': 1})
 
-    X = df['message']
+    X = df['v2']
     y = df['label']
 
     cv = CountVectorizer()
@@ -38,7 +37,7 @@ def predict():
         data = [message]
         vect = cv.transform(data).toarray()
         my_predections = clf.predict(vect)
-    return renderer_template('result.html',prediction=my_predections)
+    return render_template('result.html',prediction=my_predections)
 
 if __name__ == '__main__':
 	app.run(debug=True)
