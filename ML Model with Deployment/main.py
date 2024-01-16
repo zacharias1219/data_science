b0143113b4c27dc61f889d27e363635263347eb1
Richard Abishai <rickoshade1891@gmail.com>
2024-01-16 13:08:32 +0530

    first

---
+++ ML Model with Deployment/main.py
@@ -0,0 +1,6 @@
+import pandas as pd 
+import numpy as np 
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.metrics import classification_report
